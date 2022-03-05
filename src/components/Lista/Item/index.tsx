8d083dcc9b1f57cc0bf7fc30723324b29f58ab34
Marcelo <marcelo.ungaretti@gmail.com>
2022-03-04 23:42:16 -0300

    fim do capitulo 5

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,6 +1,13 @@
-import style from '../Lista.module.scss'
+import { ITarefa } from "../../../types/tarefa";
+import style from "../Lista.module.scss";
 
-export default function Item({tarefa, tempo}: {tarefa: string, tempo: string}) {
+export default function Item({
+  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id
+}: ITarefa) {
   return (
     <li className={style.item}>
       <h3>{tarefa}</h3>
