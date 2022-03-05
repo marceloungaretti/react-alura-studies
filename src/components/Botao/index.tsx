33705f49c68dcb8433a18c861480864620c8c40d
Marcelo <marcelo.ungaretti@gmail.com>
2022-03-04 22:02:10 -0300

    fim do capitulo 4

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import style from './Botao.module.scss'
+import style from "./Botao.module.scss";
 
 class Botao extends React.Component {
-    render() {
-        return(
-            <button className={style.Botao}>Botão</button>
-        )
-    }
+  render() {
+    return <button className={style.botao}>{this.props.children}</button>;
+  }
 }
 
 export default Botao;
